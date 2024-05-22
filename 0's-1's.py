6f3bba0b80e5c2929ceed76401fa37e52f57de84
Sumitsahani <sumitsahani.1855@gmail.com>
2024-05-22 12:29:57 +0530

    DSA

---
+++ 0's-1's.py
@@ -0,0 +1,19 @@
+N = int(input("Len : "))
+Arr = list(map(int, input("array : ").split()))
+# N=5
+# Arr =[1,0,0,1,0]
+left = 0
+right = N - 1
+
+while left < right:
+    while Arr[left] == 0 and left < right:
+        left += 1
+    while Arr[right] == 1 and left < right:
+        right -= 1
+    if left < right:
+        Arr[left] = 0
+        Arr[right] = 1
+        left += 1
+        right -= 1
+
+print(*Arr)
